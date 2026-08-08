145bac506bb40a057cf8afb5ff60ef483e395af2
Prilepskiy_AE <Woolf7754286@yandex.ru>
2026-08-08 22:03:45 +0700

    eureka

--- notification-service/src/main/java/com/prilepskiy_ae/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/prilepskiy_ae/notificationservice/NotificationServiceApplication.java
@@ -2,9 +2,11 @@ package com.prilepskiy_ae.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
